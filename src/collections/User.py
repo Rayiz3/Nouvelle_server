d32dada31e6cdc0895e07a791a1bc77cc22d7725
Rayiz3 <jusmint3@gmail.com>
2025-06-16 01:04:58 +0900

    add : field 'config'

--- src/collections/User.py
+++ src/collections/User.py
@@ -10,4 +10,9 @@ class User(BaseModel):
             'email': self.email,
             'password': self.password,
             'name': self.name,
+            'config': {
+                'color': '#ffffff',
+                'stacks': [],
+                'links': []
+            }
         }
