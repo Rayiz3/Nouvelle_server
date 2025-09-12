7d3d95da0b1e00d71891c00d3f570c0ddf7388d9
Rayiz3 <jusmint3@gmail.com>
2025-09-12 09:52:39 +0900

    add : port

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from src.routes.users import users_bp, user_bp
 
 
 app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 5000))
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -11,4 +13,4 @@ app.register_blueprint(users_bp, url_prefix='/users')
 app.register_blueprint(user_bp, url_prefix='/user')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=PORT, debug=True)
