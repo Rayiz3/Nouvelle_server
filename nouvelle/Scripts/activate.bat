9c7dced71f3f8e6c574f7566fe79f6250ef6cec3
Doun Lee <jusmint3@gmail.com>
2026-03-26 13:16:06 +0900

    remove : venv

--- nouvelle/Scripts/activate.bat
+++ nouvelle/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jusmint\Desktop\+\KAIST\2024W Madcamp\Nouvelle_server\nouvelle
+set VIRTUAL_ENV=C:\Users\jusmi\바탕 화면\+\Nouvelle\Nouvelle_server\nouvelle
 
 if not defined PROMPT set PROMPT=$P$G
 
